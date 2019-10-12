fb4fb6074b25be511add959e8ee65cade90b5519
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-10-12 21:04:41 +0800

    把数据保存到本地

--- components/header.vue
+++ components/header.vue
@@ -22,10 +22,27 @@
             <!-- 登录/用户信息 -->
             <el-row type="flex" align="middle">
 
-                <!-- 不存在用户信息展示登录注册链 -->
-                <nuxt-link to="/user/login" class="account-link">
+                <!-- 不存在用户信息展示登录注册链接 -->
+                <nuxt-link 
+                to="/user/login" 
+                class="account-link"
+                v-if="!$store.state.user.userInfo.token">
                     登录 / 注册
                 </nuxt-link>
+
+                <el-dropdown v-else>
+                    <span class="el-dropdown-link">
+                        <!-- 头像 -->
+                        <img :src="$axios.defaults.baseURL + $store.state.user.userInfo.user.defaultAvatar"/>
+                        {{ $store.state.user.userInfo.user.nickname }}
+                        <i class="el-icon-arrow-down el-icon--right"></i>
+
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>个人中心</el-dropdown-item>
+                        <el-dropdown-item>退出</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
             </el-row>
         </el-row>
     </header>
