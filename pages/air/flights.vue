d60ce095bee3266cf5aca427466caea80754198f
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-10-15 19:49:15 +0800

    使用computed实现dataList

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -47,10 +47,13 @@ export default {
     data(){
         return {
             // 请求机票列表返回的总数据，包含了flights,info,options,total 
-            flightsData: {},
+            flightsData: {
+                // 初始值
+                flights: []
+            },
 
-            // 从flights总列表数据中切割出来数组列表
-            dataList: [],
+            // // 从flights总列表数据中切割出来数组列表
+            // dataList: [],
 
             // 当前的页数
             pageIndex: 1,
@@ -64,14 +67,25 @@ export default {
         FlightsItem
     },
 
+    computed: {
+        // 从flights总列表数据中切割出来数组列表
+        dataList(){
+            const arr = this.flightsData.flights.slice(
+                (this.pageIndex - 1) * this.pageSize,
+                this.pageIndex * this.pageSize
+            )
+            return arr;
+        }
+    },
+
     methods: {
         // 分页条数切换时候触发，val是当前的条数
         handleSizeChange(val){
 
             // 切换条数
             this.pageSize = val;
-            // 重新回到第一页
-            this.dataList = this.flightsData.flights.slice(0,this.pageSize)
+            // // 重新回到第一页
+            // this.dataList = this.flightsData.flights.slice(0,this.pageSize)
         },
 
         // 页面切换时候触发，val是当前的页数
@@ -80,10 +94,10 @@ export default {
             // 修改当前的页数
             this.pageIndex = val;
             // 修改机票列表
-            this.dataList = this.flightsData.flights.slice(
-                (this.pageIndex - 1 ) * this.pageSize,
-                this.pageIndex * this.pageSize
-            )
+            // this.dataList = this.flightsData.flights.slice(
+            //     (this.pageIndex - 1 ) * this.pageSize,
+            //     this.pageIndex * this.pageSize
+            // )
         },
     },
 
@@ -97,8 +111,8 @@ export default {
             // 保存到机票的总数据
             this.flightsData = res.data;
 
-            // 第一页的数据
-            this.dataList = this.flightsData.flights.slice(0, this.pageSize)
+            // // 第一页的数据
+            // this.dataList = this.flightsData.flights.slice(0, this.pageSize)
         })
     }
 }
