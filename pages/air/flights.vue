323e23df8ef036d13230f9246573d9d7fba186a1
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-10-15 19:32:17 +0800

    分页的数据切换

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -15,7 +15,7 @@
                 <!-- 航班信息 -->
                 <!-- flightData.flights是航班的列表 -->
                 <FlightsItem
-                v-for="(item, index) in flightsData.flights"
+                v-for="(item, index) in dataList"
                 :key="index"
                 :item="item"/>
 
@@ -49,6 +49,9 @@ export default {
             // 请求机票列表返回的总数据，包含了flights,info,options,total 
             flightsData: {},
 
+            // 从flights总列表数据中切割出来数组列表
+            dataList: [],
+
             // 当前的页数
             pageIndex: 1,
             // 当前的条数
@@ -70,6 +73,13 @@ export default {
         // 页面切换时候触发，val是当前的页数
         handleCurrentChange(val){
 
+            // 修改当前的页数
+            this.pageIndex = val;
+            // 修改机票列表
+            this.dataList = this.flightsData.flights.slice(
+                (this.pageIndex - 1 ) * this.pageSize,
+                this.pageIndex * this.pageSize
+            )
         },
     },
 
@@ -82,6 +92,9 @@ export default {
         }).then(res => {
             // 保存到机票的总数据
             this.flightsData = res.data;
+
+            // 第一页的数据
+            this.dataList = this.flightsData.flights.slice(0, this.pageSize)
         })
     }
 }
