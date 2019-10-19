715d68ca5daf881b5121d50431d3f132ed63aa4f
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-10-19 21:07:05 +0800

    自定义地图点标记

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -11,8 +11,17 @@
 export default {
     mounted(){
         // 整个页面加载完毕之后执行
-        window.onload = function(){
-            var map = new AMap.Map('container');
+        window.onLoad = function(){
+            var map = new AMap.Map('container', {
+                zoom: 11,   /* 级别 */
+                center: [113.3245904, 23.1066805]   /* 中心点坐标 */
+            });
+
+            var marker = new AMap.Marker({
+                content: `<div style="width:20px; height:20px; border-radius: 50px; background:red; color:#fff; text-align:center; line-height: 20px;">99</div>`,
+                position:[113.3245904, 23.1066805]  /* 位置 */
+            })
+            map.add(marker);    /* 添加到地图 */
         }
 
         // 地图的连接
