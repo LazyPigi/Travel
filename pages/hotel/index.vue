386ebfd7d05a9ccf1be4ad58cc66acb7ba26c95b
Will-Z-fighting <53569286+Will-Z-fighting@users.noreply.github.com>
2019-10-20 17:30:08 +0800

    地图路线规划功能

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -2,26 +2,81 @@
     <div style="padding:50px;">
         <h3>高德地图</h3>
 
+    <el-row style="margin-bottom:20px;">
+        <el-col :span="5">
+            <el-input placeholder="出发地点" v-model="start"></el-input>
+        </el-col>
+        <el-col :span="5">
+            <el-input placeholder="到达地点" v-model="end"></el-input>
+        </el-col>
+        <el-button :span="2" @click="handleSearch">搜索</el-button>
+    </el-row>
+
         <!-- 地图的容器 -->
         <div id="container"></div>
+        <div id="panel"></div>
     </div>
 </template>
 
 <script>
 export default {
-    mounted(){
-        // 整个页面加载完毕之后执行
-        window.onLoad = function(){
+    data(){
+        return {
+            start: "",
+            end: ""
+        }
+    },
+
+    methods: {
+        handleSearch(){
+        this.map();
+        },
+        map(){
+        
+            // 地图对象
             var map = new AMap.Map('container', {
-                zoom: 11,   /* 级别 */
-                center: [113.3245904, 23.1066805]   /* 中心点坐标 */
+            zoom:11,//级别
+            //center: [113.3245904, 23.1066805]//中心点坐标
             });
 
-            var marker = new AMap.Marker({
-                content: `<div style="width:20px; height:20px; border-radius: 50px; background:red; color:#fff; text-align:center; line-height: 20px;">99</div>`,
-                position:[113.3245904, 23.1066805]  /* 位置 */
+            // 点标记
+            // var marker1 = new AMap.Marker({
+            //   content: `<div style="width:20px; height:20px; border-radius: 50px; background:red; color:#fff; text-align:center; line-height: 20px;">99</div>`,
+            //   position:[113.3245904, 23.1066805]//位置
+            // })
+            // var marker2 = new AMap.Marker({
+            //   position:[113.3345904, 23.1266805]//位置
+            // })
+            // var markerList = [marker1, marker2];
+            // map.add(markerList);//添加到地图
+
+            // 异步加载插件
+            AMap.plugin(['AMap.ToolBar','AMap.Driving'],() => {//异步加载插件
+            var toolbar = new AMap.ToolBar();
+            map.addControl(toolbar);
+
+            // 驾车路线的插件
+            var driving = new AMap.Driving({
+                map: map,
+                panel: "panel",
+                policy: AMap.DrivingPolicy.LEAST_TIME
+            });//驾车路线规划
+
+            var points = [
+                { keyword: this.start },
+                { keyword: this.end }
+            ]
+
+            driving.search(points, function (status, result) { 
             })
-            map.add(marker);    /* 添加到地图 */
+            });
+        }
+    },
+
+    mounted(){
+        // 整个页面加载完毕之后执行
+        window.onLoad  = () => {
+            this.map();
         }
 
         // 地图的连接
@@ -35,5 +90,24 @@ export default {
 </script>
 
 <style scoped>
-#container {width:600px; height: 500px; }  
+#container {width:600px; height: 500px;}  
+#panel {
+    position: fixed;
+    background-color: white;
+    max-height: 90%;
+    overflow-y: auto;
+    top: 10px;
+    right: 10px;
+    width: 280px;
+}
+#panel .amap-call {
+    background-color: #009cf9;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+}
+#panel .amap-lib-driving {
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    overflow: hidden;
+}
 </style>
